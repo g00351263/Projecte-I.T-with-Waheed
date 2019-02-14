8069dda5deffb177a8d77c5e1c5ae7eef7e3c0b6
raja <g00351263@gmit.ie>
2019-02-14 19:41:22 +0000

    all files missed

---
+++ users.sql
@@ -0,0 +1,7 @@
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(80) NOT NULL,
+  `name` varchar(80) NOT NULL,
+  `password` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
---
+++ waheed.md
@@ -0,0 +1,2 @@
+"# project-I.T-waheed-Raja" 
+Started Making Login page with basic html for text boxes and button
