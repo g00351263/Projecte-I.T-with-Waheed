8ee8b00331aa3e5b9f9ce1af70794fa4106866de
raja <g00351263@gmit.ie>
2019-02-14 18:57:34 +0000

    working on register page

--- register.php
+++ register.php
@@ -1,35 +1,7 @@
-<?php
-include "config.php";
-
-
-if(isset($_POST['but_submit'])){
-
-    $uname = mysqli_real_escape_string($con,$_POST['txt_uname']);
-    $password = mysqli_real_escape_string($con,$_POST['txt_pwd']);
-
-
-    if ($uname != "" && $password != ""){
-
-        $sql_query = "select count(*) as cntUser from users where username='".$uname."' and password='".$password."'";
-        $result = mysqli_query($con,$sql_query);
-        $row = mysqli_fetch_array($result);
-
-        $count = $row['cntUser'];
-
-        if($count > 0){
-            $_SESSION['uname'] = $uname;
-            header('Location: home.php');
-        }else{
-            echo "Invalid username and password";
-        }
-
-    }
-
-}
-?>
 <html>
     <head>
-        <title>Login Page</title>
+        <title>Registration Page</title>
+
         <meta name="viewport" content="width=device-width, initial-scale=1">
    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 </head>
@@ -45,11 +17,16 @@ if(isset($_POST['but_submit'])){
   <a href="login.php" class="w3-bar-item w3-button w3-mobile">Log In</a>
   <a href="about.php" class="w3-bar-item w3-button w3-mobile">About Us</a>
   <a href="contactus.php" class="w3-bar-item w3-button w3-mobile">Contact Us</a>
-</div>
+
+  </div>
         <div class="container">
             <form method="post" action="">
-      
-                    <h1>Login</h1>
+                <div id="div_login">
+                    <h1>Register</h1>
+					<div>
+                        <label>Name&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:</label><input type="text" class="textbox" id="txt_uname" name="txt_uname" placeholder="Name" />
+                    </div>
+					<br>
                     <div>
                         <label>User Name:</label><input type="text" class="textbox" id="txt_uname" name="txt_uname" placeholder="Username" />
                     </div>
@@ -58,7 +35,7 @@ if(isset($_POST['but_submit'])){
                         <label>Password &nbsp :</label><input type="password" class="textbox" id="txt_uname" name="txt_pwd" placeholder="Password"/>
                     </div>
                     <div>
-                        <input type="submit" value="Submit" name="but_submit" id="but_submit" />
+                        <input type="submit" value="Register" name="but_submit" id="but_submit" />
                     </div>
             </form>
         </div>
