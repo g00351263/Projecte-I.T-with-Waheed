bfcd0547c5fabcf047c73d2c3f85fe36422cd98d
raja <g00351263@gmit.ie>
2019-04-20 06:13:32 +0100

    confidential

--- requestReset.php
+++ requestReset.php
@@ -24,8 +24,8 @@ try {
 	// Set mailer to use SMTP
     $mail->Host       = 'smtp.gmail.com';  // Specify main and backup SMTP servers
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'rajakhan570@gmail.com';                     // SMTP username
-    $mail->Password   = 'Agnesebalode1981$';                               // SMTP password
+    $mail->Username   = '@gmail.com';                     // SMTP username
+    $mail->Password   = 'password';                               // SMTP password
     $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
     $mail->Port       = 587;                                    // TCP port to connect to
 
